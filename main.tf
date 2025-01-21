c9533839a6a484912e0c7698347695d5bc775720
Jakub <jakub.wojtowicz@sephcode.pl>
2025-01-21 10:33:23 +0100

    Initial commit
    Signed-off-by: Jakub <jakub.wojtowicz@sephcode.pl>

---
+++ main.tf
@@ -0,0 +1,8 @@
+module "spacelift-test" {
+  source  = "spacelift.dev/sephriot/spacelift-test/default"
+  version = "0.4.3"
+
+  # Optional inputs 
+  # common_length = number
+  # length        = number
+}
